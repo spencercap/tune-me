272861fab604fbdbf36fbb70e47c58ce88022680
spencercap <cappiello.spencer@gmail.com>
2020-11-04 20:38:48 +0000

    trying nova so formatting updates

--- src/App.vue
+++ src/App.vue
@@ -2,32 +2,40 @@
 	<h3>
 		<span>Scan w the Spotify app</span>
 		<br />
-		<span style="font-size: 0.8em;">Find some music</span>
+		<span style="font-size: 0.8em">Find some music</span>
 	</h3>
 
-	<div style="max-width: 300px; font-size: 13px; padding-bottom: 36px;">
-		can we discover music by randomly generating spotify codes? call it modern crate digging with a blindfold on.
-		go try and find a code that can be scanned!
+	<div
+		v-if="hello"
+		style="max-width: 300px; font-size: 13px; padding-bottom: 36px"
+	>
+		can we discover music by randomly generating Spotify codes? call it
+		modern crate digging with a blindfold on. go try and find a code that
+		can be scanned!
 	</div>
 	<!-- <HelloWorld msg="Hello Vue 3.0 + Vite" /> -->
 	<SpotCode />
 </template>
 
-
 <script lang="ts">
 import { defineAsyncComponent, defineComponent } from 'vue';
+import x from './components/HelloWorld.vue';
 
 export default defineComponent({
 	name: 'App',
 	components: {
-		HelloWorld: defineAsyncComponent(() => import('./components/HelloWorld.vue')),
-		SpotCode: defineAsyncComponent(() => import('./components/SpotCode.vue'))
+		HelloWorld: defineAsyncComponent(
+			() => import('./components/HelloWorld.vue')
+		),
+		SpotCode: defineAsyncComponent(
+			() => import('./components/SpotCode.vue')
+		),
 	},
 	data() {
 		return {
-			hello: 'world'
+			hello: 'world',
 		};
-	}
+	},
 });
 </script>
 
