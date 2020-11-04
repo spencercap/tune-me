021aa30faa54a2ddfca350d18a10d9ef9ceedaa1
spencercap <cappiello.spencer@gmail.com>
2020-11-04 14:42:24 +0000

    basic vite init

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+	<img alt="Vue logo" src="./assets/logo.png" />
+	<HelloWorld msg="Hello Vue 3.0 + Vite" />
+</template>
+
+
+<script lang="ts">
+import { defineAsyncComponent, defineComponent } from 'vue';
+
+export default defineComponent({
+	name: 'App',
+	components: {
+		HelloWorld: defineAsyncComponent(() => import('./components/HelloWorld.vue'))
+	},
+	data() {
+		return {
+			hello: 'world'
+		};
+	}
+});
+</script>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
